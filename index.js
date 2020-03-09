49895689d3e84da87ebb8429da84b71adb39d21a
ClaudzTheEngineer <claudio.m.surmon@gmail.com>
2020-03-09 18:39:33 +0200

    Added stylesheet

---
+++ index.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    //console.log(apple.name);
+    var body = document.getElementById("header");
+
+    body.style.borderColor = "#ff0000";
+}
+
+const apple = {
+    name: "bapple"
+
+}
+
+const jim = {
+    name: "jim",
+    knife: "stabby stabby" 
+}
