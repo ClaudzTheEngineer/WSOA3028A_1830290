8d513ea910cef9b4a2dfa09593b23359a8cec031
Claudio Surmon <51412047+ClaudzTheEngineer@users.noreply.github.com>
2020-04-21 08:52:02 +0200

    testing generating links

--- js/nav.js
+++ js/nav.js
@@ -3,20 +3,24 @@ function insertHeader() {
         '<img src="/WSOA3028A_1830290/images/Logo128.png" alt="Logo" style="width:100px;height:100px;"></img>';
 }
 
-function generateNav() {
-    document.getElementsByTagName("nav")[0].innerHTML +=
-        `<ul>
-            <li><a href="../index.html">Home</a></li>
-            <li><a href="index.html">About</a></li>
-            <li class="active"><a href="../blogs/index.html">Blogs</a></li>
-            <li><a href="../contact/index.html">Contact Me</a></li>
-            <li><a href="../construction/index.html">Construction Zone</a></li>
-        </ul>`;
-    console.log(window.location.pathname);
+function generateNav(active) {
+
+    let output = "<ul>";
+    let links = ["", "about/", "blogs/", "contact/", "construction/"];
+    let linkName = ["Home", "About", "Blogs", "Contact Me", "Construction Zone"];
+
+    for (let i = 0; i < links.length; i++) {
+        let currentLink = "/WSOA3028A_1830290/" + links[i] + "index.html";
+
+        output += "<li" + (i = active) ? " class=\"active\"" : "" + "><a href=\"" + currentLink + "\">" + linkName[i] + "</a></li>";
+    }
+    output += "</ul>";
+
+    document.getElementsByTagName("nav")[0].innerHTML += output;
 }
 
 
-function insertElements() {
+function insertElements(active) {
     insertHeader();
-    generateNav();
+    generateNav(active);
 }
