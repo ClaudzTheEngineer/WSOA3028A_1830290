8fe8f45a38d837865031ba518237f2800cf1c5b7
Claudio Surmon <51412047+ClaudzTheEngineer@users.noreply.github.com>
2020-04-21 08:30:18 +0200

    testing generating links

---
+++ js/nav.js
@@ -0,0 +1,22 @@
+function insertHeader() {
+    document.getElementsByTagName("header")[0].innerHTML +=
+        '<img src="../images/Logo128.png" alt="Logo" style="width:100px;height:100px;"></img>';
+}
+
+function generateNav() {
+    document.getElementsByTagName("nav")[0].innerHTML +=
+        `<ul>
+            <li><a href="../index.html">Home</a></li>
+            <li><a href="index.html">About</a></li>
+            <li class="active"><a href="../blogs/index.html">Blogs</a></li>
+            <li><a href="../contact/index.html">Contact Me</a></li>
+            <li><a href="../construction/index.html">Construction Zone</a></li>
+        </ul>`;
+    console.log(window.location.pathname);
+}
+
+
+function insertElements() {
+    insertHeader();
+    generateNav();
+}
