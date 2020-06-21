58e87b52ae7c3d5fe39b2edb3dc02d3ff7553d75
Claudio Surmon <51412047+ClaudzTheEngineer@users.noreply.github.com>
2020-06-21 20:44:02 +0200

    Started Bootstrap integration

--- js/nav.js
+++ js/nav.js
@@ -6,38 +6,116 @@ function insertHeader() {
 function generateNav(active) {
     let links = ["", "about/", "blogs/", "contact/", "services/", "construction/"];
     let linkName = ["Home", "About", "Blogs", "Contact Us", "Services", "Construction Zone"];
-    let nav = document.createElement("ul");
-    for (let i = 0; i < links.length; i++) {
-        let currentLink = "/WSOA3028A_1830290/" + links[i] + "index.html";
-        item = document.createElement("li");
-        link = document.createElement("a");
-        link.href = currentLink;
-        link.innerText = linkName[i];
-        if (active == i) link.setAttribute('class', 'active');
 
-        item.appendChild(link);
-        if (i == 2) {
-            dropdownContainer = document.createElement("div");
-            dropdownContainer.setAttribute('class', 'dropdown-content');
-            for (let index = 0; index < clamp(cards.length, 0, 6); index++) {
-                bloglink = document.createElement("a");
-                bloglink.innerText = cards[index].title;
-                bloglink.href = "/WSOA3028A_1830290/blogs/" + cards[index].link;
-                dropdownContainer.appendChild(bloglink);
+    document.getElementById("navbar").setAttribute('class', 'navbar navbar-expand-lg sticky-top bg-light navbar-light');
+
+    //Brand
+    let brand = document.createElement("a");
+    brand.setAttribute('class', 'navbar-brand');
+    brand.href = "#";
+    let logo = document.createElement("img");
+    logo.src = "/WSOA3028A_1830290/images/Trimmed-Logo.png";
+    logo.alt = "Logo";
+    logo.setAttribute('style', 'height:3rem');
+    brand.appendChild(logo);
+    document.getElementById("navbar").appendChild(brand);
+
+    //Button to Collapse
+    let button = document.createElement("button");
+    button.setAttribute('class', 'navbar-toggler');
+    button.type = "button";
+    button.setAttribute('data-toggle', 'collapse');
+    button.setAttribute('data-target', '#navbarSupportedContent');
+    button.setAttribute('aria-controls', 'navbarSupportedContent');
+    button.setAttribute('aria-expanded', 'false');
+    button.setAttribute('aria-label', 'Toggle Navigation');
+
+    let icon = document.createElement("span");
+    icon.setAttribute('class', 'navbar-toggler-icon');
+    button.appendChild(icon);
+    document.getElementById("navbar").appendChild(button);
+
+
+    //<div>
+    let navbarContainer = document.createElement('div');
+
+    navbarContainer.setAttribute('class', 'collapse navbar-collapse');
+    navbarContainer.id = "navbarSupportedContent";
+
+    //<ul>
+    let list = document.createElement('ul');
+    list.setAttribute('class', 'navbar-nav mr-auto');
+
+    //links <li>
+
+    for (let index = 0; index < links.length; index++) {
+        if (index == 2) {//Blogs
+            let item = document.createElement('li');
+            item.setAttribute('class', (index == active) ? 'nav-item active dropdown' : 'nav-item dropdown');
+
+            let dropdownToggle = document.createElement('a');
+            dropdownToggle.setAttribute('class', 'nav-link dropdown-toggle');
+            dropdownToggle.href = '#';
+            dropdownToggle.id = "navbarDropdown";
+            dropdownToggle.setAttribute('role', 'button');
+            dropdownToggle.setAttribute('data-toggle', 'dropdown');
+            dropdownToggle.setAttribute('aria-haspopup', 'true');
+            dropdownToggle.setAttribute('aria-expanded', 'false');
+            dropdownToggle.innerText = linkName[index];
+
+            item.appendChild(dropdownToggle);
+
+            let dropdownMenu = document.createElement('div');
+            dropdownMenu.setAttribute('class', 'dropdown-menu');
+            dropdownMenu.setAttribute('aria-labelledby', 'navbarDropdown');
+
+            //Top 6 blogs
+            for (let blogIndex = 0; blogIndex < 6; blogIndex++) {
+                let link = document.createElement('a');
+                link.setAttribute('class', 'dropdown-item');
+                link.href = "/WSOA3028A_1830290/blogs/" + cards[blogIndex].link;
+                link.innerText = cards[blogIndex].title;
+                dropdownMenu.appendChild(link);
             }
-            bloglistlink = document.createElement("a");
-            bloglistlink.href = "/WSOA3028A_1830290/blogs/index.html";
-            bloglistlink.innerHTML = "&#11206";
-            bloglistlink.style = "text-align: center";
-            dropdownContainer.appendChild(bloglistlink);
-            item.className += " dropdown";
-            item.appendChild(dropdownContainer);
+            //Divider
+            let divider = document.createElement('div');
+            divider.setAttribute('class', 'dropdown-divider');
+            dropdownMenu.appendChild(divider);
+
+            let mainLink = document.createElement('a');
+            mainLink.setAttribute('class', 'dropdown-item');
+            mainLink.href = "/WSOA3028A_1830290/" + links[index] + "index.html";
+            mainLink.innerText = 'See more...';
+            dropdownMenu.appendChild(mainLink);
+
+            item.appendChild(dropdownMenu);
+            list.appendChild(item);
+
+
+            list.appendChild(item);
+        } else {
+            let item = document.createElement('li');
+            item.setAttribute('class', (index == active) ? 'nav-item active' : 'nav-item');
+            let link = document.createElement('a');
+            link.setAttribute('class', 'nav-link');
+            link.href = "/WSOA3028A_1830290/" + links[index] + "index.html";;
+            link.innerText = linkName[index];
+            if (index == active) {
+                let activeSpan = document.createElement('span');
+                activeSpan.setAttribute('class', 'sr-only');
+                activeSpan.innerText = "(current)"
+                link.appendChild(activeSpan);
+            }
+            item.appendChild(link);
+            list.appendChild(item);
 
         }
 
-        nav.appendChild(item);
     }
-    document.getElementById("navbar").appendChild(nav);
+
+
+    navbarContainer.appendChild(list);
+    document.getElementById("navbar").appendChild(navbarContainer);
 }
 
 function insertFooter() {
@@ -51,6 +129,10 @@ function insertBlogNav() {
     let blogNum = path.substring(path.lastIndexOf('/') + 5);
     blogNum = parseInt(blogNum.substring(0, blogNum.lastIndexOf('.')));
 
+    let blogNav = document.getElementsByClassName("blogNav")[0];
+
+
+    /*
     let output = "";
 
     if (blogNum > 1) {
@@ -65,6 +147,8 @@ function insertBlogNav() {
     output += "<p>Blog " + blogNum + " of " + cards.length + "</p>"
     document.getElementsByClassName("blogNav")[0].innerHTML += output;
 
+    
+    */
     blogIndex = cards.length - blogNum;
     output = "";
     output += "| Published: " + cards[blogIndex].creation + " | Edited: " + cards[blogIndex].lastModified;
@@ -82,7 +166,7 @@ function insertElements(active, isBlog = false) {
         if (this.readyState == 4 && this.status == 200) {
             cards = JSON.parse(this.responseText);
 
-            insertHeader();
+            //insertHeader();
             generateNav(active);
             refreshBlogContents(active, isBlog);
             insertFooter();
@@ -104,6 +188,15 @@ function refreshBlogContents(active, isBlog = false) {
 }
 
 function CardToHTML(card) {
+    return `
+    <a class="card" style="width: 18rem;" href = "`+ card.link + `">
+        <img class="card-img-top" src="`+ card.image + `" alt="` + card.imageAlt + `">
+        <div class="card-body">
+            <h5 class="card-title">`+ card.title + `</h5>
+            <p class="card-text">`+ card.text + `</p>
+        </div>
+    </a>`
+    /*
     return `<a class="card" href="` + card.link + `">
         <img src="` + card.image + `" alt=` + card.imageAlt + `>
         <div class="container">
@@ -115,6 +208,7 @@ function CardToHTML(card) {
             </p>
         </div>
         </a>`
+        */
 }
 
 function generateLatestBlog() {
